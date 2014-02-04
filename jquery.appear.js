781031422dcb157fe061aac666d8604e1a000134
bas2k <alexander@brovikov.ru>
2014-02-04 10:35:58 +0600

    Update jquery.appear.js

--- jquery.appear.js
+++ jquery.appear.js
@@ -2,9 +2,10 @@
  * jQuery.appear
  * https://github.com/bas2k/jquery.appear/
  * http://code.google.com/p/jquery-appear/
+ * http://bas2k.ru/
  *
  * Copyright (c) 2009 Michael Hixson
- * Copyright (c) 2012 Alexander Brovikov
+ * Copyright (c) 2012-2014 Alexander Brovikov
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
  */
 (function($) {
